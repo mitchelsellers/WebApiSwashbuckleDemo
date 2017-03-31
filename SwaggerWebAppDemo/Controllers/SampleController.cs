cc333737a5ae75683005bbac66510269e886206d
Mitchel Sellers <msellers@iowacomputergurus.com>
2017-03-31 00:04:31 -0400

    Addition of complex models into process

--- SwaggerWebAppDemo/Controllers/SampleController.cs
+++ SwaggerWebAppDemo/Controllers/SampleController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
+using SwaggerWebAppDemo.Models;
 
 namespace SwaggerWebAppDemo.Controllers
 {
@@ -42,5 +43,24 @@ namespace SwaggerWebAppDemo.Controllers
             return Request.CreateResponse(HttpStatusCode.OK, "Success");
         }
 
+        /// <summary>
+        /// Method to get a listing of employers with a specified search process
+        /// </summary>
+        /// <remarks>
+        /// This is a great place to put examples for your users to see what they might need to know.
+        /// </remarks>
+        /// <param name="options">The desired search criteria for</param>
+        /// <returns>Result object with success/failure indication and any results</returns>
+        /// <response code="200">Successful response</response>
+        /// <response code="401">User not yet authenticated</response>
+        [HttpPost]
+        [Route("SearchEmployers")]
+        [Authorize]
+        [ResponseType(typeof(EmployerSearchResultModel))]
+        public HttpResponseMessage SearchEmployers(SearchRequestModel options)
+        {
+            //TODO: Actually work
+            return Request.CreateResponse(HttpStatusCode.OK, new EmployerSearchResultModel());
+        }
     }
 }
---
+++ SwaggerWebAppDemo/Models/SampleModels.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SwaggerWebAppDemo.Models
+{
+    /// <summary>
+    /// Model class for submitting search requests into the API methods
+    /// </summary>
+    public class SearchRequestModel
+    {
+        /// <summary>
+        /// The search critera that should be used.  
+        /// </summary>
+        /// <remarks>
+        /// Can use * or _ for wildcards (Note, not shown)
+        /// </remarks>
+        /// <value>User desired search (Note, not shown)</value>
+        [Required]
+        public string SearchCriteria { get; set; }
+
+        /// <summary>
+        /// Should the search include deleted records.
+        /// </summary>
+        public bool IncludeDeleted { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a response to employer search methods
+    /// </summary>
+    public class EmployerSearchResultModel
+    {
+        /// <summary>
+        /// Was this successful
+        /// </summary>
+        public bool Successful { get; set; }
+
+        /// <summary>
+        /// The listing of found employers
+        /// </summary>
+        public List<Employer> Employers { get; set; }
+    }
+
+    public class Employer
+    {
+        /// <summary>
+        /// First name of the employer
+        /// </summary>
+        public string FirstName { get; set; }
+    }
+}
