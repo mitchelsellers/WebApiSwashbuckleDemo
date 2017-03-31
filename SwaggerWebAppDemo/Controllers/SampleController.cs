2985eb26653da4bca9a8da37e6e5b1012b29d2fd
Mitchel Sellers <msellers@iowacomputergurus.com>
2017-03-30 23:42:54 -0400

    Addition of basic XML Commentary to methods

--- SwaggerWebAppDemo/Controllers/SampleController.cs
+++ SwaggerWebAppDemo/Controllers/SampleController.cs
@@ -7,9 +7,16 @@ using System.Web.Http;
 
 namespace SwaggerWebAppDemo.Controllers
 {
+    /// <summary>
+    /// Grouping of API Methods for sample/demonstration purposes
+    /// </summary>
     [RoutePrefix("Samples")]
     public class SampleController : ApiController
     {
+        /// <summary>
+        /// Sample get method, used to get a status message
+        /// </summary>
+        /// <returns>Status message.  Should be success for all responses</returns>
         [HttpGet]
         [Route("TestPublicGet")]
         public HttpResponseMessage TestPublicGet()
@@ -17,6 +24,10 @@ namespace SwaggerWebAppDemo.Controllers
             return Request.CreateResponse(HttpStatusCode.OK, "Success");
         }
 
+        /// <summary>
+        /// Sample authenticated get method, used to show a more secure method requiring authentication
+        /// </summary>
+        /// <returns>Status message. should be success for all authenticated responses</returns>
         [HttpGet]
         [Route("TestAuthenticatedGet")]
         [Authorize]
