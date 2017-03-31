52e4235659f3650aed2d4e96b5e19567c05782d4
Mitchel Sellers <msellers@iowacomputergurus.com>
2017-03-30 23:47:23 -0400

    Addition of enhanced XML comments for sample methods

--- SwaggerWebAppDemo/Controllers/SampleController.cs
+++ SwaggerWebAppDemo/Controllers/SampleController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace SwaggerWebAppDemo.Controllers
 {
@@ -17,8 +18,10 @@ namespace SwaggerWebAppDemo.Controllers
         /// Sample get method, used to get a status message
         /// </summary>
         /// <returns>Status message.  Should be success for all responses</returns>
+        /// <response code="200">Successful response</response>
         [HttpGet]
         [Route("TestPublicGet")]
+        [ResponseType(typeof(string))]
         public HttpResponseMessage TestPublicGet()
         {
             return Request.CreateResponse(HttpStatusCode.OK, "Success");
@@ -28,9 +31,12 @@ namespace SwaggerWebAppDemo.Controllers
         /// Sample authenticated get method, used to show a more secure method requiring authentication
         /// </summary>
         /// <returns>Status message. should be success for all authenticated responses</returns>
+        /// <response code="200">Successful response</response>
+        /// <response code="401">User not yet authenticated</response>
         [HttpGet]
         [Route("TestAuthenticatedGet")]
         [Authorize]
+        [ResponseType(typeof(string))]
         public HttpResponseMessage TetAuthenticatedGet()
         {
             return Request.CreateResponse(HttpStatusCode.OK, "Success");
