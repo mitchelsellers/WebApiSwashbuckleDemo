6096233a4c568c45984fc92957b763c5029e5a5f
Mitchel Sellers <msellers@iowacomputergurus.com>
2017-03-30 23:15:04 -0400

    Initial Demo Creation - Considered baseline, with only Swagger package installed

---
+++ SwaggerWebAppDemo/Controllers/SampleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SwaggerWebAppDemo.Controllers
+{
+    [RoutePrefix("Samples")]
+    public class SampleController : ApiController
+    {
+        [HttpGet]
+        [Route("TestPublicGet")]
+        public HttpResponseMessage TestPublicGet()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Success");
+        }
+
+        [HttpGet]
+        [Route("TestAuthenticatedGet")]
+        [Authorize]
+        public HttpResponseMessage TetAuthenticatedGet()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Success");
+        }
+
+    }
+}
---
+++ SwaggerWebAppDemo/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Cors;
+using System.Web.Http;
+
+[assembly: OwinStartupAttribute(typeof(SwaggerWebAppDemo.Startup))]
+namespace SwaggerWebAppDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            var config = new HttpConfiguration();
+
+            ConfigureAuth(app);
+            app.UseWebApi(config);
+        }
+    }
+}
